eaec77af941f137e2a47b234fe079a2b49ac1fbb
aryan2314 <144587572+aryan2314@users.noreply.github.com>
2025-06-01 20:37:42 +0530

    added origins

--- greencart/server/server.js
+++ greencart/server/server.js
@@ -20,7 +20,7 @@ await connectCloudinary()
 
 // Allow multiple origins
 const allowedOrigins = ['http://localhost:5173',
-  'https://greencart-alpha.vercel.app',  // your backend (self-origin)
+    // your backend (self-origin)
   'https://greencart-ollg.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
@@ -28,17 +28,7 @@ app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 // Middleware configuration
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      console.error('❌ CORS blocked origin:', origin);
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 
 app.get('/', (req, res) => res.send("API is Working"));
